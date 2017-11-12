60a571fc3d0478e959ef052c46e0104362a6c45a
SGolub <SGolub@192.168.0.33>
2017-11-12 20:05:37 +0200

    Add String Utils

---
+++ src/main/java/md/meta/Experiments.java
@@ -0,0 +1,20 @@
+package md.meta;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Experiments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String snl = null;
+		String sb = "";
+		String s = "34";
+		
+		if (StringUtils.isNotBlank(s)) {
+			System.out.print(  Integer.parseInt(s) );
+		}
+
+	}
+
+}
