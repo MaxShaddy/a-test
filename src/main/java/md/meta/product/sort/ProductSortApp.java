0deb15bc30819b27da17d09df51430184caf1d2d
SGolub <SGolub@DESKTOP-SGOLUB>
2017-11-17 16:47:46 +0200

    :)

---
+++ src/main/java/md/meta/product/sort/ProductSortApp.java
@@ -0,0 +1,47 @@
+package md.meta.product.sort;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class ProductSortApp {
+
+	public static void main(String[] args) {
+		
+		SortedMap<String, String> lhm = new ConcurrentSkipListMap<>();
+		lhm.put("000", "Zero 1");
+		lhm.put("001", "One  1");
+		lhm.put("100", "One Hundred 1");
+		lhm.put("050", "Fifthy 1");
+		lhm.put("000", "Zero 2");
+		
+		
+		Map<String, String> lhm2 = new HashMap<>();
+		
+		lhm.put("002", "Two 1");
+		lhm.put("001", "One  2");
+		lhm.put("100", "One Hundred 2");
+		lhm.put("050", "Fifthy 2");
+		lhm.put("000", "Zero 3");
+		
+		//printMap(lhm);
+		
+		
+		
+		lhm.putAll(lhm2);
+		printMap(lhm);
+		
+	}
+	
+	private static void  printMap(Map<String, String> map ) {
+		
+		long i = 0;
+		
+		for (Map.Entry<String, String> pair : map.entrySet()) {
+		    //i += pair.getKey() + pair.getValue();
+			System.out.println("[" + pair.getKey() + "] = [" + pair.getValue() + "]");
+		}
+	}
+	
+}
